6298453589c80808d85cd69a4578e5de96d7689b
ozen-hub <info.ozenhub@gmail.com>
2024-03-04 20:10:13 +0530

    1

---
+++ src/test/java/com/devstack/lms/eurekaserverapi/EurekaServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.lms.eurekaserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
